6f21c8f4c164fd5ee87bf6e4b176b60be0c203f0
pinwei67 <61811273+pinwei67@users.noreply.github.com>
2020-04-16 01:07:45 +0800

    Add files via upload

---
+++ Week6_A108060032_林品慰.py
@@ -0,0 +1,17 @@
+print("我的名字是林品慰")
+name = "林品慰"
+print("我的名字是","林品慰")
+name = input("請輸入你的名字")
+print("我的名字是",name)
+
+year = 18
+print("我今年",year,"歲")
+print("我今年18歲")
+year = input("請輸入你的年紀")
+print("我今年",year)
+
+print("我的體重44公斤")
+weight = 44
+print("我的體重",weight,"公斤")
+weight = input("請輸入你的體重")
+print("我的體重",weight)
