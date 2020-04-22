c0ebe1ec6f7a72506c313fecc35d1f03bcc81ffb
pinwei67 <61811273+pinwei67@users.noreply.github.com>
2020-04-22 23:57:34 +0800

    Add files via upload

---
+++ week7_A108060032林品慰.py
@@ -0,0 +1,25 @@
+drama_title = "梨泰院class"
+release_year = "2020"
+episode = "16" #集數
+ratings = "11.832%" #收視率
+director = "金成允"
+actor_1 = "朴敘俊" #飾朴世路
+actor_2 = "安普賢" #飾張根源
+actor_3 = "金東希" #飾張根秀
+actor_4 = "劉在明" #飾張大熙
+actress_1 = "金多美" #飾趙以瑞
+actress_2 = "權娜拉" #飾吳秀娥
+actress_3 = "李周映" #飾馬賢利
+
+print("劇名",drama_title)
+print("播出年分",release_year)
+print("集數",episode)
+print("收視率",ratings)
+print("導演",director)
+print("男主角",actor_1)
+print("男配角",actor_2)
+print("男配角",actor_3)
+print("男配角",actor_4)
+print("女主角",actress_1)
+print("女配角",actress_2)
+print("女配角",actress_3)
