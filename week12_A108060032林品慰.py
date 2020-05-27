373c4172a16fdc83d0828bbc006c6500d8527539
pinwei67 <61811273+pinwei67@users.noreply.github.com>
2020-05-27 21:47:58 +0800

    Add files via upload

---
+++ week12_A108060032林品慰.py
@@ -0,0 +1,24 @@
+# Input
+noodle = input("有麵嗎? (YES/NO)")
+sauce = input("有醬料嗎?(YES/NO) ")
+
+#Process
+if noodle == "YES":
+    if sauce == "YES":
+        print("今晚吃義大利麵")
+#Output
+print("Well done!")
+    
+# Input
+noodle = input("有麵嗎? (YES/NO)")
+sauce = input("有醬料嗎?(YES/NO) ")
+
+#Process
+if noodle == "YES" and sauce == "YES":
+    print("今晚吃義大利麵")
+    
+#Output
+print("Well done!")
+    
+    
+    
