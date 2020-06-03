983f575a0d577b0951d57e6673dc5c892f0f3ecd
pinwei67 <61811273+pinwei67@users.noreply.github.com>
2020-06-04 01:12:58 +0800

    Add files via upload

---
+++ week13_A108060032林品慰.py
@@ -0,0 +1,25 @@
+score = int(input("請輸入成績: "))
+if score >= 80:
+     print("非常好")
+elif score >=60:   
+     print("繼續保持")
+else:    
+     print("再加油喔")
+     
+score=int(input("請輸入成績"))
+if score<60:
+    print("再加油喔")
+else:
+    print("繼續保持")
+if score>=80:
+    print("非常好")
+    print("還可以再努力一下喔")
+    
+score=int(input("請輸入成績"))
+if score>=80:
+    print("非常好")
+else:
+    if score >=60:
+        print("繼續保持")
+    else:
+        print("再加油喔")    
